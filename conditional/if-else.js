c9bb7213306641ff3798c98b5177c74ae956aec0
andri689 <andriandwi0529@gmail.com>
2023-12-18 12:15:05 +0700

    feat: conditional in js

---
+++ conditional/if-else.js
@@ -0,0 +1,26 @@
+const bil = 4;
+let answer;
+
+if (bil == 9) {
+    answer = 'ini bilangan ganjil';
+} else if (bil == 4) {
+    answer = 'ini bilangan genap';
+} else {
+    answer = 'bukan bilangan'
+}
+
+if ("kondisi") {
+    //apa yang mau dijalankan 
+} else if ("kondisi2") {
+    //apa yang mau dijalankan 
+} else {
+    //kode yang mau dijalankan
+}
+
+if (meetSchedule == 9) {
+    answer = "ini meet pagi kita";
+} else if (meetSchedule ==13) {
+    answer = "ini meet siang kita";
+} else {
+    answer = "itu bukan jadwal meet kita";
+}
